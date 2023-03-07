913fdb3d179dcc5b8386062b42a59a28d7b74e1f
Jeffrey Schwartz <jefftschwartz@gmail.com>
2023-03-07 09:53:04 -0500

    Addresses issue #19: Include a total for number of pages generated in console output.

--- src/core/services/serialize.ts
+++ src/core/services/serialize.ts
@@ -8,31 +8,34 @@ import { _outputFile } from "../lib/io/_outputFile.js";
 import { _writeJSONFile } from "../lib/io/_writeJSONFile.js";
 import { _filter } from "../lib/functional.js";
 import { getConfiguration } from "./configuration/getConfiguration.js";
-import type { Asset, Assets } from "../../types/types";
+import type { Assets } from "../../types/types";
 import * as metrics from "../lib/metrics.js";
 
 /**
- * Serialize every template's content except those that are used to generate a collection.
+ * Serialize every template's content except for those that are
+* collection generators and return the total number of pages serialized.
  */
 
-const serializePages = async function(assets: Assets) {
+const serializePages = async function(assets: Assets): Promise<number> {
     const buildFolder = (await getConfiguration()).buildFolder;
     const buildFolderPath = path.join(process.cwd(), buildFolder);
     _remove(buildFolder);
     const templateAssets: Assets = _filter(assets, asset => asset.assetType === "template" && !asset.fm?.data["isCollection"]);
+    let count = 0;
     for (const asset of templateAssets) {
         const outputPath = path.join(buildFolderPath, asset.htmlDocumentName as string);
         if (typeof asset.content === "undefined") continue;
         await _outputFile(outputPath, asset.content);
+        count++;
     }
-    return;
+    return count;
 };
 
 /**
- * Serialize assets as JSON.
+ * Serialize assets to assets.json.
  */
 
-const serializeAssets = async function(assets: Assets) {
+const serializeAssets = async function(assets: Assets): Promise<void> {
     const outputPath = path.join(process.cwd(), "assets.json");
     await _writeJSONFile(outputPath, assets);
     return;
@@ -40,7 +43,8 @@ const serializeAssets = async function(assets: Assets) {
 
 export const serialize = async function(assets: Assets): Promise<Assets> {
     metrics.startTimer("serialization");
-    await serializePages(assets);
+    const count = await serializePages(assets);
+    console.log("total pages generated: ", count);
     await serializeAssets(assets);
     metrics.stopTimer("serialization");
     return assets;
