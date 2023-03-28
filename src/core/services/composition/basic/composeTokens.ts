06f3838c1b3acacc4481a98d13270f1effe39a06
Jeffrey Schwartz <jefftschwartz@gmail.com>
2023-03-28 18:02:30 -0400

    Addresses issue #38: baseURL is being applied when building for development.

--- src/core/services/composition/basic/composeTokens.ts
+++ src/core/services/composition/basic/composeTokens.ts
@@ -11,9 +11,7 @@ const getUerConfigBaseURL = async function(): Promise<string | undefined> {
 };
 
 export const composeTokens = async function(assetContent: string, tokens: Tokens): Promise<string> {
-    // if (typeof tokens === "undefined") return assetContent;
-    const baseURL = await getUerConfigBaseURL();
-    // If it exists, apply the user's baseURL.
+    const baseURL = process.env["BUILD_STRATEGY"] === "DEVELOPMENT" ? "" : await getUerConfigBaseURL();
     const _tokens = typeof baseURL !== "undefined" ? { ...tokens, baseURL } : tokens;
     for (const [key, value] of Object.entries(_tokens)) {
         assetContent = findAndReplaceTokenContent(assetContent, `{${key}}`, value as string);
