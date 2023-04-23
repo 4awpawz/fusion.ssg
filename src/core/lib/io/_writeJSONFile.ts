566a4237f325f271c17b80b1903ee660e375155d
Jeffrey Schwartz <jefftschwartz@gmail.com>
2023-04-23 10:46:54 -0400

    Added 'options' as a parameter.

--- src/core/lib/io/_writeJSONFile.ts
+++ src/core/lib/io/_writeJSONFile.ts
@@ -5,9 +5,9 @@
 import chalk from "chalk";
 import fs from "fs-extra";
 
-export const _writeJSONFile = async function(path: string, object: unknown): Promise<void> {
+export const _writeJSONFile = async function(path: string, object: unknown, options = {}): Promise<void> {
     try {
-        await fs.writeJson(path, object);
+        await fs.writeJson(path, object, options);
     } catch (error) {
         console.error(chalk.red(`there was an error: When writing JSON file ${path}.`));
         throw error;
