553ea3a64a88041aafac622e09edf74bbcc35e45
Jeffrey Schwartz <jefftschwartz@gmail.com>
2023-04-27 16:43:59 -0400

    Addresses isue #56: Support wips without paths.

--- src/core/services/discovery/templateIsWIP.ts
+++ src/core/services/discovery/templateIsWIP.ts
@@ -3,14 +3,15 @@
  */
 
 export const templateIsWIP = function(wips: string[], templatePath: string) {
+    const tPath = templatePath.split("/").slice(2).join("/"); // Remove src/templates from templatePth.
     for (const wip of wips) {
         const isIgnore = wip[0] === "!";
         const _wip = isIgnore ? wip.substring(1) : wip;
         const wipIsAFolder = _wip.length > 0 && _wip.endsWith("/");
-        if (wipIsAFolder && templatePath.startsWith(_wip)) {
+        if (wipIsAFolder && tPath.startsWith(_wip)) {
             return isIgnore ? false : true;
         }
-        if (_wip === templatePath) {
+        if (_wip === tPath) {
             return isIgnore ? false : true;
         }
     }
