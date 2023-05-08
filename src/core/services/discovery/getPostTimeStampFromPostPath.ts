cb2cc39fe464b7b2f7881d1751f094004bbb47bd
Jeffrey Schwartz <jefftschwartz@gmail.com>
2023-05-08 14:40:01 -0400

    Address issue #59: Generated time stamp for post date is off by 1 day.

--- src/core/services/discovery/getPostTimeStampFromPostPath.ts
+++ src/core/services/discovery/getPostTimeStampFromPostPath.ts
@@ -1,10 +1,11 @@
 /**
- * getPostTimeStamp - Returns the date of a post, taken from its file path, and return, and return the date as a timestamp, i.e. in milliseconds.
+ * getPostTimeStamp - Returns the date of a post, taken from its file path, as a timestamp, i.e. in milliseconds.
  */
 
 export const getPostTimeStampFromPostPath = function(path: string): number {
     const regex = /(\d{4})-(\d{2})-(\d{2})/;
     const result = path.match(regex);
     if (result === null) return 0;
-    return Date.parse(result[0]) as number;
+    const date = result[0].replaceAll("-", "/"); // Without this the date will be off by 1 day.
+    return Date.parse(date) as number;
 };
