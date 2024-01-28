885296779bc096abda216ae17e434984fa0d9781
Jeffrey Schwartz <jefftschwartz@gmail.com>
2024-01-27 21:02:58 -0500

    Addresses issue #111: cache.ts::readCache can inadvertently return undefined if path isn't in the cache and the file doesn't exist.

--- src/core/lib/cache.ts
+++ src/core/lib/cache.ts
@@ -5,6 +5,7 @@
  */
 
 import { parse } from "path";
+import chalk from "chalk";
 import { _fileExists } from "./io/_fileExists.js";
 import { _readFile } from "./io/_readFile.js";
 import { _readJSONFile } from "./io/_readJsonFile.js";
@@ -12,7 +13,7 @@ import { _readJSONFile } from "./io/_readJsonFile.js";
 const rCache = new Map();
 
 const cacheJSONFile = async function(path: string): Promise<Map<unknown, unknown>> {
-    return rCache.set(path, _readJSONFile(path));
+    return rCache.set(path, await _readJSONFile(path));
 };
 
 const cacheFile = async function(path: string): Promise<Map<unknown, unknown>> {
@@ -23,10 +24,13 @@ export const deleteCachedFile = function(path: string): void {
     rCache.delete(path);
 };
 
-export const readCache = async function(path: string): Promise<unknown> {
-    !rCache.has(path) && _fileExists(path) &&
-        (parse(path).ext === ".json" && cacheJSONFile(path) || cacheFile(path));
-    return rCache.get(path);
+export const readCache = async function(path: string): Promise<string | object> {
+    const fileExists = _fileExists(path);
+    fileExists && !rCache.has(path) && (parse(path).ext === ".json" && await cacheJSONFile(path) || await cacheFile(path));
+    if (fileExists) return rCache.get(path);
+    // Theoretically the following should never occur but better to handle it just in case.
+    console.log(chalk.red(`there was an error: File ${path} does not exist and aborting process.`));
+    process.abort();
 };
 
 export const clearCache = function(): void {
