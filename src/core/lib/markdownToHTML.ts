3703fda2d7484f4a899b691eef1e1eed7005e294
Jeffrey Schwartz <jefftschwartz@gmail.com>
2025-02-09 14:08:34 -0500

    Addresses issue #121: Use the marked-gfm-heading-id module to generate IDs for markdown headers

--- src/core/lib/markdownToHTML.ts
+++ src/core/lib/markdownToHTML.ts
@@ -3,7 +3,9 @@
  */
 
 import { marked } from "marked";
+import { gfmHeadingId } from "marked-gfm-heading-id";
 
 export const markdownToHTML = async function(content: string) {
+    marked.use(gfmHeadingId({}));
     return marked.parse(content);
 };
